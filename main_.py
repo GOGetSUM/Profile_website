d81cc100af78f4e692e13adf41061bff048c1afe
Nick <testcorona0429@gmail.com>
2022-04-16 17:03:33 -0700

    Database update

--- main_.py
+++ main_.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -9,8 +11,8 @@ from wtforms.validators import DataRequired, email_validator
 
 app = Flask(__name__)
 #--------------------create new data base for messages---------------------------------
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///visitors.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Bootstrap(app)
